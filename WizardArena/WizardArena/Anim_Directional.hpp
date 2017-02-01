a34e1f48797860e738cfc6fed21f8491bf34c08c
Drost <drostd4564@uwstout.edu>
2017-02-01 10:42:53 -0600

    Removed Duplicate Files

--- WizardArena/WizardArena/Anim_Directional.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Anim_Base.hpp"
-
-class Anim_Directional : public Anim_Base {
-protected:
-	void frameStep();
-	void cropSprite();
-	void readIn(std::stringstream& stream);
-};
--- WizardArena/WizardArena/Observer.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Message.hpp"
-
-class Observer {
-public:
-	virtual ~Observer() {}
-	virtual void notify(const Message& message) = 0;
-};
